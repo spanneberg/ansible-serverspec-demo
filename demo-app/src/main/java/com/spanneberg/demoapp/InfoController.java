e4b61531d73f85b6a12e4daca2ec2bd10dba59e5
Bastian Spanneberg <bastian@spanneberg.com>
2015-10-02 11:35:38 +0200

    renamed controller class

--- demo-app/src/main/java/com/spanneberg/demoapp/InfoController.java
+++ demo-app/src/main/java/com/spanneberg/demoapp/InfoController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class GreetingController {
+public class InfoController {
 
     @RequestMapping("/info")
     public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) throws UnknownHostException, SocketException {
