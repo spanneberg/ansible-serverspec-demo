3d4b634197b82412f6ffec34b5ce1ea97e502473
Bastian Spanneberg <bastian@spanneberg.com>
2015-10-02 12:59:35 +0200

    added Bootstrap for CSS

--- demo-app/src/main/java/com/spanneberg/demoapp/InfoController.java
+++ demo-app/src/main/java/com/spanneberg/demoapp/InfoController.java
@@ -1,23 +1,19 @@
 package com.spanneberg.demoapp;
 
 import java.net.InetAddress;
-import java.net.NetworkInterface;
 import java.net.SocketException;
 import java.net.UnknownHostException;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class InfoController {
 
     @RequestMapping("/info")
-    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) throws UnknownHostException, SocketException {
-    	String address = NetworkInterface.getByName("eth1").getInetAddresses().nextElement().getHostAddress();
+    public String info(Model model) throws UnknownHostException, SocketException {
         String hostname = InetAddress.getLocalHost().getHostName();
-        model.addAttribute("address", address);
         model.addAttribute("hostname", hostname);
         return "info"; 
     }
