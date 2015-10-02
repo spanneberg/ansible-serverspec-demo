e5b263f29723209372b13868624a1c686a3413be
Bastian Spanneberg <bastian@spanneberg.com>
2015-10-02 16:38:02 +0200

    added manifest data to info page. updated gradle wrapper.

--- demo-app/src/main/java/com/spanneberg/demoapp/InfoController.java
+++ demo-app/src/main/java/com/spanneberg/demoapp/InfoController.java
@@ -8,13 +8,19 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.jcabi.manifests.Manifests;
+
 @Controller
 public class InfoController {
 
     @RequestMapping("/info")
     public String info(Model model) throws UnknownHostException, SocketException {
         String hostname = InetAddress.getLocalHost().getHostName();
+        String appName = Manifests.read("Application-Name");
+        String appVersion = Manifests.read("Application-Version");
         model.addAttribute("hostname", hostname);
+        model.addAttribute("appName", appName);
+        model.addAttribute("appVersion", appVersion);
         return "info"; 
     }
 
