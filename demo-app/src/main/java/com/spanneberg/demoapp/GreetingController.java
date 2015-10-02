7b8f58452dadca5b1de10b469c8b8bef4a929877
Bastian Spanneberg <bastian@spanneberg.com>
2015-10-02 11:34:39 +0200

    added demo-app

---
+++ demo-app/src/main/java/com/spanneberg/demoapp/GreetingController.java
@@ -0,0 +1,25 @@
+package com.spanneberg.demoapp;
+
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class GreetingController {
+
+    @RequestMapping("/info")
+    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) throws UnknownHostException, SocketException {
+    	String address = NetworkInterface.getByName("eth1").getInetAddresses().nextElement().getHostAddress();
+        String hostname = InetAddress.getLocalHost().getHostName();
+        model.addAttribute("address", address);
+        model.addAttribute("hostname", hostname);
+        return "info"; 
+    }
+
+}
